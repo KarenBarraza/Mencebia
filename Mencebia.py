5e366c8c60997aa44d302dfc62c0ae9fb9c369f3
Michelle Adriana Mogollon Uriza <Mich>
2025-12-03 11:21:39 -0500

    Feat: Archivo donde se llaman y ejecutan las funciones, Funcion seleccionar columnas funcionando. Esta funcion solo seleeciona la columna alias y las columnas Genero de cliente y Tipo de servicio

--- Mencebia.py
+++ Mencebia.py
@@ -1,7 +1,29 @@
-import pandas as pd
-import matplotlib.pyplot as mp
+import pandas
 from Archivo import Archivo
+import Funciones
 
-archivo= Archivo('Buceo_de_la_Concha.csv')
-archivo.leer()
-print(archivo.datos)
+def menu():
+    archivo = Archivo("Buceo_de_la_Concha.csv")
+    archivo.leer()
+    df=archivo.datos
+    while True:
+        print("""
+===========================
+            MENÚ
+===========================
+1. Seleccionar columnas
+2. Seleccionar filas
+3. Aplicar filtros
+4. Salir
+===========================
+""")
+
+        opcion = input("Elige una opción: ")
+
+        if opcion == "1":
+            Funciones.seleccionar_columnas(df)
+        if opcion == "2":
+            Funciones.seleccionar_filas(df)
+        
+        
+menu()
