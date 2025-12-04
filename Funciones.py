00bed2267b86fe2143fbf5ecf0bf4af1c1ce77db
Michelle Adriana Mogollon Uriza <Michi>
2025-12-04 08:59:34 -0500

    feat:creacion del la funcion valores_nulos en el archivo funciones y su agregacion al menu en el archivo mencebia

--- Funciones.py
+++ Funciones.py
@@ -1,29 +1,48 @@
 import pandas as pd
 from Archivo import Archivo
+class Funciones:
+    def __init__(self):
+           pass
+           
 
-def seleccionar_columnas(df):
-    print("\n--- Selección de columnas ---")
+    def seleccionar_columnas(self, df):
+            print("\n--- Selección de columnas ---")
 
-    print("\nColumna 'alias':")
-    print(df['alias'])
+            print("\nColumna 'alias':")
+            print(df['alias'])
 
-    print("\nColumnas 'genero_cliente' y 'tipo_servicio':")
-    print(df[['genero_cliente', 'tipo_servicio']])
-
-
-def seleccionar_filas(df):
-    print("\n--- Selección de filas ---")
-    print(df.iloc[0:5])
+            print("\nColumnas 'genero_cliente' y 'tipo_servicio':")
+            print(df[['genero_cliente', 'tipo_servicio']])
 
 
-def aplicar_filtros(df):
-    print("\n--- Aplicación de filtros ---")
+    def seleccionar_filas(self,df):
+            print("\n--- Selección de filas ---")
+            print(df.iloc[0:5])
 
-    clientes_femeninos = df[df['genero_cliente'] == 'Femenino']
-    print("\nClientes Femeninos:\n", clientes_femeninos)
 
-    altos = df[df['valor_total'] > 300000]
-    print("\nRegistros con valor_total > 300000:\n", altos)
+    def aplicar_filtros(self, df):
+            print("\n--- Aplicación de filtros ---")
 
-    print("\nCantidad de clientes femeninos:", len(clientes_femeninos))
-    print("Cantidad de registros con valor_total > 300000:", len(altos))
+            clientes_femeninos = df[df['genero_cliente'] == 'Femenino']
+            print("\nClientes Femeninos:\n", clientes_femeninos)
+
+            altos = df[df['valor_total'] > 300000]
+            print("\nRegistros con valor_total > 300000:\n", altos)
+
+            print("\nCantidad de clientes femeninos:", len(clientes_femeninos))
+            print("Cantidad de registros con valor_total > 300000:", len(altos))
+
+    def valores_nulos(self, df):
+           print("--- Manejo de valores nulos ---")
+           df.loc[100, "tiempo_minutos"]=None
+           print(df.isnull().sum())
+           df["tiempo_minutos"]=df["tiempo_minutos"].fillna(1)
+           #df["tiempo_minutos"].fillna(1, inplace=True)
+           print(df.isnull().sum())
+           df["Recalcular_total"] = df["valor_total"] / df["tiempo_minutos"]
+           print(df["Recalcular_total"])
+
+
+
+
+    
--- Mencebia.py
+++ Mencebia.py
@@ -1,7 +1,7 @@
 import pandas
 from Archivo import Archivo
-import Funciones
-
+from Funciones import Funciones
+funcion=Funciones()
 def menu():
     archivo = Archivo("Buceo_de_la_Concha.csv")
     archivo.leer()
@@ -14,19 +14,22 @@ def menu():
 1. Seleccionar columnas
 2. Seleccionar filas
 3. Aplicar filtros
-4. Salir
+4. valores nulos
+5. Salir
 ===========================
 """)
 
         opcion = input("Elige una opción: ")
 
         if opcion == "1":
-            Funciones.seleccionar_columnas(df)
+            funcion.seleccionar_columnas(df)
         if opcion == "2":
-            Funciones.seleccionar_filas(df)
+            funcion.seleccionar_filas(df)
         elif opcion == "3":
-            Funciones.aplicar_filtros(df)
+            funcion.aplicar_filtros(df)
         elif opcion == "4":
+            funcion.valores_nulos(df)
+        elif opcion == "5":
             print("Saliendo del programa...")
             break
         
