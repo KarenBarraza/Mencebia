f4a67a77f992b3f0b5859db32743f585c7a3c1de
Michelle Adriana Mogollon Uriza <Michi>
2025-12-04 09:16:53 -0500

    Feat: Se comentarea completamente el codigo para obtener una mayor claridad , se dejo el menu listo para agrear otra funcion

--- Archivo.py
+++ Archivo.py
@@ -1,10 +1,13 @@
+#Importacion de la libreria pandas
 import pandas as pd
 
+#creacion de la clase archivo
 class Archivo:
+#se define la ruta
     def __init__(self, ruta):
         self.ruta = ruta
         self._datos = None
-
+#funcion que lee el archivo CSV
     def leer(self):
         try:
             self.datos = pd.read_csv(self.ruta)
@@ -12,11 +15,11 @@ class Archivo:
 
         except FileNotFoundError:
             print("No existe el archivo en la ruta indicada.")
-
+#Getter que returna datos
     @property
     def datos(self):
         return self._datos
-
+#setter para datos
     @datos.setter
     def datos(self, datos):
         self._datos = datos
--- Mencebia.py
+++ Mencebia.py
@@ -1,11 +1,14 @@
+#Importacion de todos  los archivos necesarios
 import pandas
 from Archivo import Archivo
 from Funciones import Funciones
 funcion=Funciones()
+#funcion que define el menu y los atributos a usar
 def menu():
     archivo = Archivo("Buceo_de_la_Concha.csv")
     archivo.leer()
     df=archivo.datos
+#Creacion del menu
     while True:
         print("""
 ===========================
@@ -15,12 +18,13 @@ def menu():
 2. Seleccionar filas
 3. Aplicar filtros
 4. valores nulos
+5. Rango valor minuto
 5. Salir
 ===========================
 """)
-
+#Permite seleccionar una opcion
         opcion = input("Elige una opción: ")
-
+#Selecciona la  opcion y ejecuta la funcion determinada
         if opcion == "1":
             funcion.seleccionar_columnas(df)
         if opcion == "2":
@@ -30,6 +34,8 @@ def menu():
         elif opcion == "4":
             funcion.valores_nulos(df)
         elif opcion == "5":
+            continue
+        elif opcion == "6":
             print("Saliendo del programa...")
             break
         
