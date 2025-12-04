cb9e22de03e9e455a890a4e09541ed92bd95172c
Alison Kamila Reyes Baquero <alisonreyesuwu.com>
2025-12-04 09:17:25 -0500

    feat:Se hizo la creacion de tres funciones, la primera nos permite ver las primeras cinco columnas, la segunda nos permite ver el tipo de datos, la tercera los permite ver el valor minimo y la cuarta nos permite ver el valor maximo.Por ultimo se agregaron las funciones al menu y se comentario todo el codigo

--- Mencebia.py
+++ Mencebia.py
@@ -1,3 +1,4 @@
+#Importacion de pandas y clases
 import pandas as pd
 import matplotlib.pyplot as plt
 from MencebiaClass import MencebiaClass
@@ -5,31 +6,46 @@ from Archivo import Archivo
 # Ruta del archivo CSV
 instanciacion= Archivo("Buceo_de_la_Concha.csv")
 instanciacion.leer()
+#Intanciacion del datafream
 df=instanciacion.datos
 mencebia = MencebiaClass()
-
+#Creacion del bucle del menu
 while True:
     print("Seleccione una opción:")
     print("1. Mostrar ordenado el total")
     print("2. Mostrar total por producto")
     print("3. Mostrar total por alias")
     print("4. Salir")
-    
+    print("5. Mostrar las primeras filas")
+    print("6. Mostrar tipo de datos")
+    print("7. Mostrar valor minimo")
+    print("8. Mostrar valor maximo")
+#Campo donde se ingresa la opcion que desea el usuario
     opcion = input("Ingrese el número de la opción deseada: ")
     
     mencebia = MencebiaClass()
-    
+#La opcion uno que nos muestra el orden de mayor a menor
     if opcion == '1':
         print(mencebia.ordenado_total(df))
-
+#La opcion dos que nos muestra el total por porducto
     elif opcion == '2':
         print(mencebia.total_por_producto(df))
-
+#La opcion tres que nos muestra el total por alias
     elif opcion == '3':
         print(mencebia.total_por_alias(df))
-
+#La opcion cuatro que rompre el bucle para cerrar el programa
     elif opcion == '4':
         print("Saliendo del programa.")
         break
-    else:
-        print("Opción no válida. Por favor, intente de nuevo.")
+#La opcion cinco que nos muestra las primeras cinco filas
+    elif opcion =='5' :
+        print(mencebia.visualizacion_filas(df))
+#La opcion seis que nos muestra el tipo de datos
+    elif opcion == '6':
+        print(mencebia.tipo_datos(df))
+#La opcion siete que nos muestra el valor minimo
+    elif opcion == '7' :
+        print("El valor minimo es: ",mencebia.valor_minimo(df))
+#La opcion ocho que nos muestra el valor maximo
+    elif opcion == '8':
+        print("El valor maximo es: ",mencebia.valor_maximo(df))
